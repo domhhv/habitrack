4c02a87c7c7be0daea885d749989e4d02d5341e5
Dominik Hryshaiev <domhryshaiev@gmail.com>
2024-08-25 11:26:31 +0200

    fix(calendar): start week on monday on mobile

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -9,6 +9,7 @@ import { supabaseClient, theme } from '@helpers';
 import { CssVarsProvider } from '@mui/joy';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import React from 'react';
+import { I18nProvider } from 'react-aria';
 
 type ProviderProps = {
   children: React.ReactNode;
@@ -20,20 +21,22 @@ const Providers = ({ children, rangeStart, rangeEnd }: ProviderProps) => {
   return (
     <CssVarsProvider defaultMode="system" theme={theme}>
       <SessionContextProvider supabaseClient={supabaseClient}>
-        <SnackbarProvider>
-          <UserAccountProvider>
-            <TraitsProvider>
-              <HabitsProvider>
-                <OccurrencesProvider
-                  rangeStart={rangeStart}
-                  rangeEnd={rangeEnd}
-                >
-                  {children}
-                </OccurrencesProvider>
-              </HabitsProvider>
-            </TraitsProvider>
-          </UserAccountProvider>
-        </SnackbarProvider>
+        <I18nProvider locale="en-GB">
+          <SnackbarProvider>
+            <UserAccountProvider>
+              <TraitsProvider>
+                <HabitsProvider>
+                  <OccurrencesProvider
+                    rangeStart={rangeStart}
+                    rangeEnd={rangeEnd}
+                  >
+                    {children}
+                  </OccurrencesProvider>
+                </HabitsProvider>
+              </TraitsProvider>
+            </UserAccountProvider>
+          </SnackbarProvider>
+        </I18nProvider>
       </SessionContextProvider>
     </CssVarsProvider>
   );
