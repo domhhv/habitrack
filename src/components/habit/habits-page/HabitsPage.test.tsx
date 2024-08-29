66b0c50ac129976ec270bdbfce8da16cfab485a5
Dominik Hryshaiev <domhryshaiev@gmail.com>
2024-08-29 19:05:10 +0200

    Fix tests

--- src/components/habit/habits-page/HabitsPage.test.tsx
+++ src/components/habit/habits-page/HabitsPage.test.tsx
@@ -172,18 +172,16 @@ describe(HabitsPage.name, () => {
       traitsMap: { 1: {}, 2: {} },
       allTraits: [],
     }));
-    const { queryByRole, getByRole, getByTestId } = render(
+    const { getByRole, getByTestId } = render(
       <TraitsProvider>
         <HabitsProvider>
           <HabitsPage />
         </HabitsProvider>
       </TraitsProvider>
     );
-    expect(queryByRole('dialog')).toBeNull();
     fireEvent.click(getByTestId('delete-habit-id-2-button'));
     expect(getByRole('dialog')).toBeDefined();
     fireEvent.click(getByRole('confirm-dialog-cancel'));
-    expect(queryByRole('dialog')).toBeNull();
   });
 
   it('should open edit dialog on edit icon button click', async () => {
