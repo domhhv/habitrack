7a00eec0535f7550e3bb66ba038c912016262991
Dominik Hryshaiev <domhryshaiev@gmail.com>
2024-10-25 21:32:58 +0200

    feat(calendar): round borders

--- src/components/calendar/CalendarMonthGrid.tsx
+++ src/components/calendar/CalendarMonthGrid.tsx
@@ -1,4 +1,5 @@
 import { type CalendarDate, getWeeksInMonth } from '@internationalized/date';
+import clsx from 'clsx';
 import React, { type ForwardedRef } from 'react';
 import { useLocale } from 'react-aria';
 import { type CalendarState } from 'react-stately';
@@ -23,46 +24,74 @@ const Month = (
   const weekIndexes = [...new Array(weeksInMonthCount).keys()];
   const { month: activeMonth } = state.visibleRange.start;
 
+  const getCellPosition = (weekIndex: number, dayIndex: number) => {
+    if (weekIndex === 0 && dayIndex === 0) {
+      return 'top-left';
+    }
+
+    if (weekIndex === 0 && dayIndex === 6) {
+      return 'top-right';
+    }
+
+    if (weekIndex === weeksInMonthCount - 1 && dayIndex === 0) {
+      return 'bottom-left';
+    }
+
+    if (weekIndex === weeksInMonthCount - 1 && dayIndex === 6) {
+      return 'bottom-right';
+    }
+
+    return '';
+  };
+
   return (
     <div ref={ref} className="flex flex-1 flex-col">
-      {weekIndexes.map((weekIndex) => (
-        <div
-          key={weekIndex}
-          className="flex h-[110px] justify-between border-l-2 border-r-2 border-t-2 border-neutral-500 last-of-type:border-b-2 dark:border-neutral-400 lg:h-auto"
-        >
-          {state
-            .getDatesInWeek(weekIndex)
-            .map((calendarDate: CalendarDate | null) => {
-              if (!calendarDate) {
-                return null;
-              }
+      {weekIndexes.map((weekIndex) => {
+        const weekContainerClassName = clsx(
+          'flex h-[110px] justify-between border-l-2 border-r-2 border-t-2 border-neutral-500 last-of-type:border-b-2 dark:border-neutral-400 lg:h-auto',
+          weekIndex === 0 && 'rounded-t-lg',
+          weekIndex === weeksInMonthCount - 1 && 'rounded-b-lg'
+        );
 
-              const { month, day, year } = calendarDate;
+        return (
+          <div key={weekIndex} className={weekContainerClassName}>
+            {state
+              .getDatesInWeek(weekIndex)
+              .map((calendarDate: CalendarDate | null, dayIndex) => {
+                if (!calendarDate) {
+                  return null;
+                }
 
-              const rangeStatus =
-                month < activeMonth
-                  ? 'below-range'
-                  : month > activeMonth
-                    ? 'above-range'
-                    : 'in-range';
+                const { month, day, year } = calendarDate;
 
-              const [dayKey] = calendarDate.toString().split('T');
+                const rangeStatus =
+                  month < activeMonth
+                    ? 'below-range'
+                    : month > activeMonth
+                      ? 'above-range'
+                      : 'in-range';
 
-              return (
-                <CalendarCell
-                  key={dayKey}
-                  dateNumber={day}
-                  monthNumber={month}
-                  fullYear={year}
-                  onClick={onDayModalDialogOpen}
-                  rangeStatus={rangeStatus}
-                  onNavigateBack={state.focusPreviousPage}
-                  onNavigateForward={state.focusNextPage}
-                />
-              );
-            })}
-        </div>
-      ))}
+                const [dayKey] = calendarDate.toString().split('T');
+
+                const position = getCellPosition(weekIndex, dayIndex);
+
+                return (
+                  <CalendarCell
+                    key={dayKey}
+                    dateNumber={day}
+                    monthNumber={month}
+                    fullYear={year}
+                    onClick={onDayModalDialogOpen}
+                    rangeStatus={rangeStatus}
+                    position={position}
+                    onNavigateBack={state.focusPreviousPage}
+                    onNavigateForward={state.focusNextPage}
+                  />
+                );
+              })}
+          </div>
+        );
+      })}
     </div>
   );
 };
