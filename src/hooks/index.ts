fe60ac7d7a4d45ab7fac15ff43eec8e09145c24b
Dominik Hryshaiev <domhryshaiev@gmail.com>
2025-03-04 16:24:56 +0100

    refactor: deprecate useDocumentTitle in favor of react 19 title tag

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -6,5 +6,4 @@ export { default as useTextField } from './useTextField';
 export { default as useFileField } from './useFileField';
 export { default as useDataFetch } from './useDataFetch';
 export { default as useFetchOnAuth } from './useFetchOnAuth';
-export { default as useDocumentTitle } from './useDocumentTitle';
 export { default as useUser } from './useUser';
--- src/hooks/useDocumentTitle.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const useDocumentTitle = (title: string) => {
-  React.useEffect(() => {
-    document.title = title;
-  }, [title]);
-};
-
-export default useDocumentTitle;
