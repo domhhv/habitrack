b3bd6ace0f40a2f6d0d8e25a053684ef1e74371a
Dominik Hryshaiev <domhryshaiev@gmail.com>
2025-03-09 19:56:36 +0100

    chore(db): change accounts rls read policy

---
+++ supabase/migrations/20250309173929_change_accounts_rls_read_policy.sql
@@ -0,0 +1,9 @@
+ALTER POLICY "Enable read access for all users"
+ON "public"."accounts"
+TO authenticated
+USING (
+  (SELECT auth.uid()) = id
+);
+ALTER POLICY "Enable read access for all users"
+ON "public"."accounts"
+RENAME TO "Enable users to view their own data only";
