c14c4150f221aab9e1e9247b3ed3313965137588
Dominik Hryshaiev <domhryshaiev@gmail.com>
2025-03-09 20:26:54 +0100

    perf(db): fix auth RLS initialization plan issue for notes table

---
+++ supabase/migrations/20250309192416_prevent_notes_auth_rls_init_plan.sql
@@ -0,0 +1,34 @@
+drop policy "Enable insert for users based on user_id" on "public"."notes";
+
+drop policy "Enable delete for users based on user_id" on "public"."notes";
+
+drop policy "Enable read access for users based on user_id" on "public"."notes";
+
+drop policy "Enable update for users based on user_id" on "public"."notes";
+
+create policy "Enable delete for users based on user_id"
+on "public"."notes"
+as permissive
+for delete
+to public
+using ((( SELECT auth.uid() AS uid) = user_id));
+
+
+create policy "Enable read access for users based on user_id"
+on "public"."notes"
+as permissive
+for select
+to public
+using ((( SELECT auth.uid() AS uid) = user_id));
+
+
+create policy "Enable update for users based on user_id"
+on "public"."notes"
+as permissive
+for update
+to public
+using ((( SELECT auth.uid() AS uid) = user_id))
+with check ((( SELECT auth.uid() AS uid) = user_id));
+
+
+
