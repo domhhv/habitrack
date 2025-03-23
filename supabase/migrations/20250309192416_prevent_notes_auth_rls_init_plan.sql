fd251682e727681ae40630f2447fae4d9957d12a
Dominik Hryshaiev <domhryshaiev@gmail.com>
2025-03-23 14:12:14 +0100

    style(db): lint supabase migrations sql (#197)
    * style(db): lint supabase migrations sql
    
    * style: test linting failure
    
    * ci(sqlfluff): lint entire supabase folder
    
    * style: undo changes for testing linting failure and delete sql conversion script

--- supabase/migrations/20250309192416_prevent_notes_auth_rls_init_plan.sql
+++ supabase/migrations/20250309192416_prevent_notes_auth_rls_init_plan.sql
@@ -1,34 +1,29 @@
-drop policy if exists "Enable insert for users based on user_id" on "public"."notes";
+DROP POLICY IF EXISTS "Enable insert for users based on user_id" ON "public"."notes";
 
-drop policy "Enable delete for users based on user_id" on "public"."notes";
+DROP POLICY "Enable delete for users based on user_id" ON "public"."notes";
 
-drop policy "Enable read access for users based on user_id" on "public"."notes";
+DROP POLICY "Enable read access for users based on user_id" ON "public"."notes";
 
-drop policy "Enable update for users based on user_id" on "public"."notes";
+DROP POLICY "Enable update for users based on user_id" ON "public"."notes";
 
-create policy "Enable delete for users based on user_id"
-on "public"."notes"
-as permissive
-for delete
-to public
-using ((( SELECT auth.uid() AS uid) = user_id));
+CREATE POLICY "Enable delete for users based on user_id"
+ON "public"."notes"
+AS PERMISSIVE
+FOR DELETE
+TO "public"
+USING (((SELECT "auth"."uid"() AS "uid") = "user_id"));
 
+CREATE POLICY "Enable read access for users based on user_id"
+ON "public"."notes"
+AS PERMISSIVE
+FOR SELECT
+TO "public"
+USING (((SELECT "auth"."uid"() AS "uid") = "user_id"));
 
-create policy "Enable read access for users based on user_id"
-on "public"."notes"
-as permissive
-for select
-to public
-using ((( SELECT auth.uid() AS uid) = user_id));
-
-
-create policy "Enable update for users based on user_id"
-on "public"."notes"
-as permissive
-for update
-to public
-using ((( SELECT auth.uid() AS uid) = user_id))
-with check ((( SELECT auth.uid() AS uid) = user_id));
-
-
-
+CREATE POLICY "Enable update for users based on user_id"
+ON "public"."notes"
+AS PERMISSIVE
+FOR UPDATE
+TO "public"
+USING (((SELECT "auth"."uid"() AS "uid") = "user_id"))
+WITH CHECK (((SELECT "auth"."uid"() AS "uid") = "user_id"));
