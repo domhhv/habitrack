db6b218172df3ee7eef3170903833c177efca45c
Dominik Hryshaiev <domhryshaiev@gmail.com>
2025-05-20 23:45:10 +0200

    refactor: deprecate tailwind config helper (#269)

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,2 @@
 export { default as handleAsyncAction } from './handle-async-action';
 export { default as supabaseClient } from './supabase-client';
-export { default as tailwindConfig } from './tailwind-config';
