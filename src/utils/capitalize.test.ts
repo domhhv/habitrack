2fb8c44571221187a9e8fa1d66034c7e049a35ca
Dominik Hryshaiev <domhryshaiev@gmail.com>
2025-08-28 16:18:42 +0200

    refactor: use lodash.capitalize instead of app util

--- src/utils/capitalize.test.ts
+++
@@ -1,9 +0,0 @@
-import { it, expect, describe } from 'vitest';
-
-import capitalize from './capitalize';
-
-describe(capitalize.name, () => {
-  it('should capitalize the first letter of a string', () => {
-    expect(capitalize('hello')).toBe('Hello');
-  });
-});
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,3 @@
-export { default as capitalize } from './capitalize';
 export { default as getErrorMessage } from './get-error-message';
 export { default as toEventLike } from './to-event-like';
 export { default as toHashMap } from './to-hash-map';
