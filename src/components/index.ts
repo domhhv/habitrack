285a153f6bee3ef98068ef81a619f601d4652502
Dominik Hryshaiev <domhryshaiev@gmail.com>
2025-12-15 22:18:32 +0100

    refactor(components): flatten directories (#305)

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,6 @@ export * from './calendar';
 export * from './common';
 export * from './habit';
 export * from './header';
+export * from './note';
+export * from './occurrence';
 export * from './trait';
---
+++ src/components/note/index.ts
@@ -0,0 +1,1 @@
+export { default as NoteDialog } from './NoteDialog';
