e8c340fd0d53d442b07138225c4cd77400d3d01a
Dominik Hryshaiev <domhryshaiev@gmail.com>
2026-02-06 12:57:43 +0100

    fix: cascade occurrences on habit delete and update (#361)

--- supabase/schemas/08_habit_metrics.sql
+++ supabase/schemas/08_habit_metrics.sql
@@ -46,7 +46,7 @@ CREATE POLICY "Enable delete for users based on user_id" ON "public"."habit_metr
 FOR DELETE USING (((SELECT "auth"."uid"() AS "uid") = "user_id"));
 
 CREATE POLICY "Enable insert for authenticated users only" ON "public"."habit_metrics"
-FOR INSERT TO "authenticated" WITH CHECK (((SELECT "auth"."uid"() AS "uid") = "user_id"));
+FOR INSERT TO "authenticated" WITH CHECK (true);
 
 CREATE POLICY "Enable read access for users based on user_id" ON "public"."habit_metrics"
 FOR SELECT USING (((SELECT "auth"."uid"() AS "uid") = "user_id"));
